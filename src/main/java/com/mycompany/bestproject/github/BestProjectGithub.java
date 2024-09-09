aa98add2885611a2df2d5a790edc1a9198fd148f
RC_Student_lab <RC_Student_lab@10.0.0.147>
2024-09-09 08:23:29 +0000

    Created a program to add two numbers

--- src/main/java/com/mycompany/bestproject/github/BestProjectGithub.java
+++ src/main/java/com/mycompany/bestproject/github/BestProjectGithub.java
@@ -11,6 +11,11 @@ package com.mycompany.bestproject.github;
 public class BestProjectGithub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       int a = 5;
+       int b= 10;
+       int c= a + b;
+     
+    System.out.println("Results : + int c");
+    
     }
 }
